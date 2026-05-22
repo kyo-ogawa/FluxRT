1e5104e93e8f0ff7c52e08933e2de93c2e6d12ca
FluxRT Dev <fluxrt@local>
2026-05-22 19:03:37 +0900

    build: CMakeLists.txt for FluxRTTop.dll and placeholder headers

---
+++ td_plugin/src/FluxRTTop.cpp
@@ -0,0 +1,4 @@
+// FluxRTTop.cpp - TouchDesigner C++ TOP plugin (placeholder)
+// This will be implemented in Task 8+
+
+#include "FluxRTTop.h"
---
+++ td_plugin/src/FluxRTTop.h
@@ -0,0 +1,6 @@
+// FluxRTTop.h - TouchDesigner C++ TOP plugin (placeholder)
+// This will be implemented in Task 8
+
+#pragma once
+
+// Placeholder for full implementation in Task 8
